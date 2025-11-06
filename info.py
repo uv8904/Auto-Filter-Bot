6b5a992d50a237cdf79242d8a3615a0262a6fe99
uv8904 <uvjangra2@gmail.com>
2025-11-06 20:03:30 +0530

    Set BOT_TOKEN to a predefined token
    Updated BOT_TOKEN to a specific value.

--- info.py
+++ info.py
@@ -17,7 +17,7 @@ def is_enabled(value, default):
 SESSION = environ.get('SESSION', 'media_search')
 API_ID = int(environ.get('API_ID', '20803355'))
 API_HASH = environ.get('API_HASH', 'caa85d91bcde4e8826ad697de02af771')
-BOT_TOKEN = environ.get('BOT_TOKEN', "")
+BOT_TOKEN = environ.get('BOT_TOKEN', "8548428672:AAEb7nf8hgBPLu17hme69lO_jXxr3JT_D9E")
 
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
 USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', True))
